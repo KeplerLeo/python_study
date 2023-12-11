8c5da575889dea57dd33395b34c9a012004e7fbc
Leonardo Kepler <leokepler@gmail.com>
2023-12-11 20:09:18 -0300

    feat: add function get_command_options to spotify.py

--- exercises/section2/day1/spotify.py
+++ exercises/section2/day1/spotify.py
@@ -4,3 +4,11 @@ from typing import Dict, List
 from rich import print
 
 DATA_PATH = "exercises/section2/day1/spotify.csv"
+
+
+def get_command_options() -> str:
+    return """
+    The following commands are available:
+    1 - Show top 10 most popular instrumentals songs
+    2 - Show top 10 most popular dance songs
+    3 - Show top 10 most popular energetic songs"""
