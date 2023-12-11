cbc0184b6d2b954bcd2731de12d1881686a836a7
Leonardo Kepler <leokepler@gmail.com>
2023-12-11 19:39:03 -0300

    feat: add function lines_with_word_occurrences to word_finder.py

--- exercises/section2/day1/word_finder.py
+++ exercises/section2/day1/word_finder.py
@@ -2,3 +2,12 @@ def read_file_lines(file_path):
     with open(file_path, "r") as file:
         file_content = file.read()
     return file_content.split("\n")
+
+
+def lines_with_word_occurrences(file_path: str, word: str):
+    str_lines = read_file_lines(file_path)
+    result = []
+    for index, line in enumerate(str_lines, start=1):
+        if word.casefold() in line.casefold():
+            result.append(index)
+        return result
