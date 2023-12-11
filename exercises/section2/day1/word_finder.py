270c9a4c2466361126bbcc0bda1a1f531d1197b2
Leonardo Kepler <leokepler@gmail.com>
2023-12-11 19:54:30 -0300

    feat: finishing word_finder.py

--- exercises/section2/day1/word_finder.py
+++ exercises/section2/day1/word_finder.py
@@ -10,4 +10,15 @@ def lines_with_word_occurrences(file_path: str, word: str):
     for index, line in enumerate(str_lines, start=1):
         if word.casefold() in line.casefold():
             result.append(index)
-        return result
+    return result
+
+
+def main():
+    file_path = "exercises/section2/day1/word_finder.py"
+    search_word = "python"
+    occurrences = lines_with_word_occurrences(file_path, search_word)
+    print(f'Word "{search_word}" occurs in lines:', occurrences)
+
+
+if __name__ == "__main__":
+    main()
